93635ee739d474d3e0dcfecf936f4a3355e37f36
landoyjx <lando@bihao.pro>
2020-11-10 21:43:57 +0800

    Create create2.sol

---
+++ create2.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.5.16;
+
+interface IERC20 {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+}
+
+contract Wallet {
+    constructor(address token, address hotWallet) public {
+        // send all tokens from this contract to hotwallet
+        IERC20(token).transfer(
+            hotWallet,
+            IERC20(token).balanceOf(address(this))
+        );
+        // selfdestruct to receive gas refund and reset nonce to 0
+        selfdestruct(address(0x0));
+    }
+}
+
+contract Fabric {
+    function createContract(uint256 salt) public {
+        // get wallet init_code
+        bytes memory bytecode = type(Wallet).creationCode;
+        assembly {
+            let codeSize := mload(bytecode) // get size of init_bytecode
+            let newAddr := create2(
+                0, // 0 wei
+                add(bytecode, 32), // the bytecode itself starts at the second slot. The first slot contains array length
+                codeSize, // size of init_code
+                salt // salt from function arguments
+            )
+        }
+    }
+}
