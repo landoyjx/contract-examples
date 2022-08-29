c7774840fb5544ed72f4db41aa61e188a42b9bd5
landoyjx <landoyjx@qq.com>
2022-08-29 15:31:00 +0800

    Update create2.sol

--- create2.sol
+++ create2.sol
@@ -17,27 +17,43 @@ interface IERC20 {
 }
 
 contract Wallet {
-    constructor(address token, address hotWallet) public {
+    constructor(address token, address to) public {
         // send all tokens from this contract to hotwallet
-        IERC20(token).transfer(
-            hotWallet,
-            IERC20(token).balanceOf(address(this))
-        );
+        uint256 balance = IERC20(token).balanceOf(address(this));
+        require(balance > 0, "!needed");
+        require(IERC20(token).transfer(
+            to,
+            balance
+        ), "collect failed");
         // selfdestruct to receive gas refund and reset nonce to 0
         selfdestruct(address(0x0));
     }
 }
 
 contract Fabric {
-    function createContract(uint256 salt) public {
-        // get wallet init_code
+
+    function getBytecode(address token, address to) public pure returns (bytes memory) {
         bytes memory bytecode = type(Wallet).creationCode;
+        return abi.encodePacked(bytecode, uint256(token), uint256(to));
+    }
+
+    function getAddress(address token, address to, uint256 salt) public view returns (address output) {
+        bytes memory bytecode = getBytecode(token, to);
+        output = address(uint(keccak256(abi.encodePacked(
+                hex'ff',
+                address(this), // creator
+                abi.encodePacked(salt), // salt
+                keccak256(bytecode) // init code hash
+            ))));
+    }
+
+    function collect(address token, address to, uint256 salt) external returns (address wallet) {
+        bytes memory bytecode = getBytecode(token, to);
         assembly {
-            let codeSize := mload(bytecode) // get size of init_bytecode
-            let newAddr := create2(
+            wallet := create2(
                 0, // 0 wei
                 add(bytecode, 32), // the bytecode itself starts at the second slot. The first slot contains array length
-                codeSize, // size of init_code
+                mload(bytecode), // size of init_code
                 salt // salt from function arguments
             )
         }
